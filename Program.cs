7ed3924e7b490e0135c1049ae2a9214a6b4ab757
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-06 23:07:24 +0530

    [kirti]Add. Adding @

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Email.validateEmail();
+        }
+    }
+}
