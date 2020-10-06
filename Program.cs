30e50ce7fe5831492b0d5af49ffcaf2dae92c198
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-06 22:56:06 +0530

    [kirti]Add. initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Email.validateEmail();
+        }
+    }
+}
