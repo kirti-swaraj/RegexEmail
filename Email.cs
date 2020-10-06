012b5dc8f004ae92f67f14b0a75589a85df01cef
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-06 23:14:09 +0530

    [kirti]Add. Adding .

---
+++ Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexEmail
+{
+    class Email
+    {
+        public static void validateEmail()
+        {
+            string emailPattern = "^[a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]+$";
+            Regex regex = new Regex(emailPattern);
+            Console.WriteLine("Please enter email:");
+            string email = Console.ReadLine();
+            if (regex.IsMatch(email))
+                Console.WriteLine("It is a valid email");
+            else
+                Console.WriteLine("It is not a valid email");
+
+        }
+    }
+}
